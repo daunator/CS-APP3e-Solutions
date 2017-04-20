6af677aaf805c9906f149d4813eb1c9d18e3bda8
Gabriel Francu <gabi.francu@gmail.com>
2017-04-20 17:46:27 +0300

    Add solution to homework problem 3.62

--- Homework/Chapter_03/main.c
+++ Homework/Chapter_03/main.c
@@ -3,12 +3,14 @@
 #include <stdio.h>
 
 typedef __int128 int128_t;
+typedef enum {MODE_A, MODE_B, MODE_C, MODE_D, MODE_E} mode_t;
 
 /* function prototypes */
 long decode2(long x, long y, long z);
 void store_prod(int128_t *dest, int64_t x, int64_t y);
 long loop(long, long);
 long cred_alt(long *xp);
+long switch3(long *p1, long *p2, mode_t action);
 
 int main()
 {
@@ -23,8 +25,13 @@ int main()
   loop(1l, 2l);
 
   /*** 3.61 ***/
-  long l;
-  cred_alt(&l);
+  long l1;
+  cred_alt(&l1);
+
+  /*** 3.63 ***/
+  long l2;
+  switch3(&l1, &l2, MODE_E);
+
   return 0;
 }
 
